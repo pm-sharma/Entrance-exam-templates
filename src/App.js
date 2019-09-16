1a514408c9becae135335670bd94bf28903829ce
pm-sharma <pmsharma1973990@gmail.com>
2019-09-17 00:00:12 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Iitjeetempl from './components/Iitjeetempl.tsx';
+
+function App() {
+  return (
+    <Iitjeetempl />
+  );
+}
+
+export default App;
